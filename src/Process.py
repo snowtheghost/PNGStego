cf85c70cbf458c3db073dc799df79de8ce7df55c
Justin Chan <csnow.to@gmail.com>
2022-01-21 01:41:45 -0500

    update

--- src/Process.py
+++ src/Process.py
@@ -10,13 +10,13 @@ class Process:
         pass
 
     def encode(self):
-        png_reader = PNGReader('../resources/HiddenFeelings000_Shape.png')
+        png_reader = PNGReader('../resources/Untitled_Artwork 8.PNG')
         # with open('../resources/text.txt', 'r') as file:
         #     text = file.read()
         # reader = StringReader(text)
-        reader = FileReader('../resources/HiddenFeelings000_Payload.png')
+        reader = FileReader('../resources/Payload.mp3')
         png_encoder = PNGEncoder(png_reader.get_array(), png_reader.get_data(), reader.get_message())
-        png_encoder.encode_file_except([8, 5, 2])
+        png_encoder.encode_file_except([16, 11, 19])
         png_writer = PNGWriter(png_encoder.get_array(), png_reader.get_data())
         png_writer.write()
 
@@ -24,7 +24,7 @@ class Process:
         png_reader = PNGReader('../resources/test_output.png')
         png_decoder = PNGDecoder(png_reader.get_array(), png_reader.get_data())
         # png_decoder.decode_except([0, 0, 0])
-        png_decoder.decode_file_except([8, 5, 2])  # TODO: More selective algorithm
+        png_decoder.decode_file_except([16, 11, 19])  # TODO: More selective algorithm
 
 
 if __name__ == "__main__":
